155c062a67f6a039fdde6d8ea08ca400cce5a6bb
Fernando-Ake-Maas <80129774+Fernando-Ake-Maas@users.noreply.github.com>
2022-04-07 12:43:44 -0500

    Create StarGame.java

---
+++ StarGame.java
@@ -0,0 +1,44 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class StartGame extends JDialog {
+
+    public StartGame(JFrame frame, boolean modal) {
+        super(frame, modal);
+        initComponets();
+    }
+
+    public void initComponets() {
+        this.getContentPane().setLayout(new FlowLayout());
+
+        //
+        imgFondo = new JLabel();
+        imgFondo.setIcon(new ImageIcon(getClass().getResource("/recursos/cone/Laberinto.png")));
+        imgFondo.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                imgFondoMouseClicked(e);
+            }
+        });
+
+        add(imgFondo);
+
+        // Perzonalizacion de la Ventana
+        this.setSize(810, 400);
+        this.setUndecorated(true);
+        this.setBackground(new Color(0, 0, 0, 0));
+        this.setLocationRelativeTo(null);
+        this.setOpacity(0.9f);
+        this.setVisible(true);
+    }
+
+    public void imgFondoMouseClicked(MouseEvent e) {
+        this.dispose();
+        new MainGame().initComponents();
+    }
+    
+    /// Variables declaration
+    JLabel imgFondo;
+    // End of variables declaration
+
+}
