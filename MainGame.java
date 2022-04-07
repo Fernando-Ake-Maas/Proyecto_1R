5d0924198cf95884a4ebe5d8a0bbb23445102468
Fernando-Ake-Maas <80129774+Fernando-Ake-Maas@users.noreply.github.com>
2022-04-07 12:26:18 -0500

    Create MainGame.java

---
+++ MainGame.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class MainGame {
+
+
+    public void initComponents() {
+        JFrame ventana = new JFrame();
+        Laberinto lab = new Laberinto();
+
+        ventana.setSize(new Dimension(940, 560));
+        ventana.setTitle("Laberinto");
+        ventana.setLocationRelativeTo(null);
+        ventana.setResizable(false);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.add(lab);
+        ventana.setVisible(true);
+    }
+}
