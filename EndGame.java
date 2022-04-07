99c2260cc67b3891fc89fc1f9f52e6d7621952f4
Fernando-Ake-Maas <80129774+Fernando-Ake-Maas@users.noreply.github.com>
2022-04-07 12:23:03 -0500

    Create EndGame.java

---
+++ EndGame.java
@@ -0,0 +1,47 @@
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class EndGame extends JDialog{
+
+    public EndGame(JFrame owner, boolean modal) {
+        super(owner, modal);
+        initComponets();
+    }
+
+    public void initComponets(){
+        this.getContentPane().setLayout(new FlowLayout());
+
+        //
+        imgFondo = new JLabel();
+        imgFondo.setIcon(new ImageIcon(getClass().getResource("/recursos/cone/GameOver.png")));
+        imgFondo.addMouseListener(new MouseAdapter(){
+            public void mouseClicked(MouseEvent e) {
+                imgFondoMouseClicked(e);
+            }
+        });
+
+        add(imgFondo);
+
+        // Perzonalizacion de la Ventana
+        this.setSize(810, 400);
+        this.setUndecorated(true);
+        this.setBackground(new Color(0, 0, 0, 0));
+        this.setLocationRelativeTo(null);
+        this.setOpacity(0.9f);
+        this.setVisible(true);
+    }
+
+    public void imgFondoMouseClicked(MouseEvent e){
+        System.out.println("Presionado");
+        this.dispose();
+        StartGame startGame = new StartGame(new JFrame(), true);
+        this.dispose();
+    }
+
+    /// Variables declaration
+    JLabel imgFondo;
+    // End of variables declaration
+    
+}
