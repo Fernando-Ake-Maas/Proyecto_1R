aec7d7c01f267f377ca2dd7fe02f032b87cdc7ac
Fernando-Ake-Maas <80129774+Fernando-Ake-Maas@users.noreply.github.com>
2022-04-07 12:24:19 -0500

    Create InitGame.java

---
+++ InitGame.java
@@ -0,0 +1,17 @@
+package ejercicios.Proyecto;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Fernando Ake
+ */
+public class InitGame {
+
+    public static void main(String[] args) {
+
+        StartGame startGame = new StartGame(new JFrame(), false);
+
+    }
+
+}
